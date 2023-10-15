0e76211495e2eb8c3c6a4c237044a8df036da277
Alfie Collins <107699866+ConjecturesandRefutations@users.noreply.github.com>
2023-10-15 13:34:41 +0100

    Introduction

---
+++ index.py
@@ -0,0 +1,14 @@
+###Introduction###
+
+#Portability: The open source nature of Python makes it portable and can be ported to many platforms without requiring any changes
+#E.g. Linux, Windows, Solaris, Playstation, Sharp Zaurus and more
+#High productivity: Python is a good choice if you want to develop complex multi-protocol network applications
+
+#It can connect with database systems and can read and modify files
+#It supports rapid prototyping and production completed software development
+#It is an interpreted language which means that the interpreter implements the code line by line at a single time
+#It can be integrated with languages like C,JAVA,C++, easily
+#It is a very easy to learn language,that makes it user-friendly
+#It is a dynamically-typed language.This means that the type for a value is decided at runtime, not in advance
+
+#It is used in web-development, data analysis, education
